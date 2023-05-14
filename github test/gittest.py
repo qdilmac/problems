0a21a194cad0de3f1829ca80b880aa99fb28f20a
qdilmac <mustafaosmandilmac@gmail.com>
2023-05-15 02:18:30 +0300

    first commit

---
+++ github test/gittest.py
@@ -0,0 +1,8 @@
+# learning github repos / push / pull / commit / merge / branch / clone / fork / pull request / etc
+# https://www.youtube.com/watch?v=SWYqp7iY_Tc
+
+def main():
+    print("main function has been called")
+
+if __name__ == "__main__":
+    main()
